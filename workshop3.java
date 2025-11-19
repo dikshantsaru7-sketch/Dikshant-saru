6f821eab2eaa7f27e33e36d7e58b358f5f5cb012
Dikshant <dikshantsaru7@gmail.com>
2025-11-19 12:44:58 +0545

    hhhh

---
+++ workshop3.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class workshop3 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class workshop3
+{ 
+    public static void main(String[] args){
+        System.out.println("dikshant");
+        
+        
+    }
+}
