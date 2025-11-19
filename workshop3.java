277b4b2e59f6ef59aeb354c89c25c9fcdd4f78c8
Dikshant <dikshantsaru7@gmail.com>
2025-11-19 13:03:30 +0545

    new

--- workshop3.java
+++ workshop3.java
@@ -7,8 +7,24 @@
  */
 public class workshop3
 { 
+    int ages; //instance variable
+        static int age ;// static variable
+        
+    
+    
     public static void main(String[] args){
+        
+        int qty=10; //local varibale
         System.out.println("dikshant");
+        
+        //<workshop3> <variable>=new <workshop3>();
+        workshop3 w1= new workshop3();
+         System.out.println(w1.age);
+         System.out.println(age);
+         
+         System.out.println("dikshant");
+         
+        
         
         
     }
